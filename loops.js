a4636270461309d1164238e21dfd033ff98dae0c
tjmax79 <ogidantunji@gmail.com>
2024-09-09 15:51:13 +0100

    array codes

---
+++ loops.js
@@ -0,0 +1,78 @@
+//  while loop
+
+// let x = 0
+
+// while(x < 10){
+//     console.log("i am blessed", x)
+//     x++
+// }
+
+// while(x<1000){
+//     console.log(x+1, "we are programmer")
+//     x++
+// }
+
+//  Do while loop
+// do{
+//     console.log('i ran')
+// }while(x < 0)
+
+
+// lets check password
+
+// const pin = '1234'
+// let userPin
+// let y = 0
+
+// do{
+//     userPin = prompt("Enter pin:")
+//     y++
+//     if(y===3 && userPin !== pin){
+//         alert('too many trials')
+//     }
+// }while (userPin !== pin && y < 3)
+
+    // // FOR LOOPS
+
+    // for (let e = 0; e<10; e++){
+    //     console.log(e,"I ate");
+    // }
+
+    // // ARRAY
+    // const numb= [23,36,78,95,34,56]
+    // console.log(numb);
+    // for(let i = 0; i< numb.length; i++){
+    //     console.log(numb[i]);
+    // }
+
+    // let i = 0
+
+    // while (i <numb.lengh){
+    //     console.log(numb[i]);
+    //     i++
+    // }
+
+    // class work(to find odd and even number from 1- 10)
+    // for (i = 1; i <= 10; i++){
+        
+    //     if(i % 2){
+    //         console.log(i,"is odd");
+            
+    //     }else{
+    //         console.log(i, "is even")
+    //     }
+        
+    // }
+    
+
+    //   test on array using splice and slice, pop, push,unshift and shift
+    // const arr = [10,20,30,40,50,60,70,80,90]
+   
+    //   arr.splice(5,1)
+    //   arr.splice(5,0,4)
+      
+
+    //   console.log(arr. length);
+
+
+ 
